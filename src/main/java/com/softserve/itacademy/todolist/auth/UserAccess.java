2ab4afc4f88c1a2475f1b9741815903a56ad36fb
ElnurKasimov <ekasimov2012@gmail.com>
2023-08-30 10:08:02 +0300

    refactored security rules in UserController

---
+++ src/main/java/com/softserve/itacademy/todolist/auth/UserAccess.java
@@ -0,0 +1,21 @@
+package com.softserve.itacademy.todolist.auth;
+
+import com.softserve.itacademy.todolist.model.User;
+import com.softserve.itacademy.todolist.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component()
+public class UserAccess {
+
+    @Autowired
+    private UserService userService;
+
+    public boolean isOwner(long id) {
+        User user = userService.readById(id);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return user.getEmail().equals(authentication.getName());
+    }
+}
